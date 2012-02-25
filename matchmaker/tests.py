33cbf9c56fa17f22d46183012467bfdd777afa02
Carles Barrobés <carles@barrobes.com>
2012-02-25 23:00:35 +0000

    Rename to matchmaker

--- matchmaker/tests.py
+++ matchmaker/tests.py
@@ -20,8 +20,23 @@ def is_four(item):
 def is_five(item):
     return item == 5
 
+@match_maker
+def is_even(item):
+    """Is even"""
+    return item % 2 == 0
+
 def test_is_four():
     assert_that(4, is_four())
 
 def test_is_five():
     assert_that(5, is_five())
+
+def test_is_five_or_four():
+    assert_that(4, any_of(is_four(), is_five()))
+
+def _is_five_or_even(item):
+    assert_that(item, any_of(is_five(), is_even()))
+
+def test_is_five_or_even():
+    for value in range(10):
+        yield _is_five_or_even, value
