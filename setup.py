b23defc4afd0b05cf6d029fbe55013a710accef2
Carles Barrobés <carles@barrobes.com>
2012-02-26 22:19:08 +0000

    Improve example in setup.py

--- setup.py
+++ setup.py
@@ -19,9 +19,24 @@ Examples::
         return item % 2 == 0
 
     @match_maker
-        def ends_like(item, data, length=3):
+    def ends_like(item, data, length=3):
         "String whose last {1} chars match those for '{0}'"
         return item.endswith(data[-length:])
+
+You can then use these in your tests as::
+
+    assert_that(number, is_even())
+    assert_that(word, ends_like(other_word, 4))
+
+Errors will display as::
+
+    AssertionError:
+    Expected: Is even
+         but: was <3>
+
+    AssertionError:
+    Expected: String whose last 4 chars match those for 'cello'
+         but: was 'hullo'
 """
 
 setup(
